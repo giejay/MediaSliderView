f1181f410fecff8490f1adc61e1994d38b8cc0ee
giejay <gertjancom@gmail.com>
2025-06-05 22:18:49 +0200

    Add FileName, FilePath + Camera to MetaData options Add sorting and filtering (by content type, image/video) in all views.

--- mediaslider/src/main/kotlin/nl/giejay/mediaslider/model/MetaDataType.kt
+++ mediaslider/src/main/kotlin/nl/giejay/mediaslider/model/MetaDataType.kt
@@ -1,14 +1,14 @@
 package nl.giejay.mediaslider.model
 
 enum class MetaDataType(val title: String, val defaultFontSize: Int) {
-    CLOCK("Clock", 48),
-    MEDIA_COUNT("Media Count", 18),
-    DESCRIPTION("Description", 18),
-    COUNTRY("Country", 18),
-    CITY("City", 18),
     ALBUM_NAME("Album Name", 18),
-    DATE("Date", 18)
-    // add camera (make + model in exifInfo)
-    // add originalPath
-    // add originalFileName
+    CAMERA("Camera Model", 18),
+    CITY("City", 18),
+    CLOCK("Clock", 48),
+    COUNTRY("Country", 18),
+    DATE("Date", 18),
+    DESCRIPTION("Description", 18),
+    FILENAME("Filename", 18),
+    FILEPATH("Filepath", 18),
+    MEDIA_COUNT("Media Count", 18)
 }
